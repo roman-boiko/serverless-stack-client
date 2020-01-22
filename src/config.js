c7f3af80e9026eebdb8a6f7f844db7547a488f90
Roman Boiko <romboiko@amazon.de>
2020-01-22 22:00:48 +0300

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-central-1",
+        BUCKET: "rboiko-notes-app-uploads"
+    },
+    apiGateway: {
+        REGION: "eu-central-1",
+        URL: "https://ea92qtaxj6.execute-api.eu-central-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "eu-central-1",
+        USER_POOL_ID: "eu-central-1_Gcx5x7MsD",
+        APP_CLIENT_ID: "3acblmmhik7ah35vr41daemrl2",
+        IDENTITY_POOL_ID: "eu-central-1:aaad88a7-9d17-4f83-aca3-b57b4ff2e6d4"
+    }
+};
